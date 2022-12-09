d75f211aec2b27c7bab94b1f423e0a20c5e9fbee
berger <robertberger.dev@gmail.com>
2022-12-09 12:15:17 -0500

    Added if statment that verifies topping entry

--- script.js
+++ script.js
@@ -29,8 +29,10 @@ function calculateTotal(toppingArray) {
 
     // For Loop
     for(var i = 0; i < toppings.length; i++){
+       if (toppings[i] != "") {
         total = total + toppingCost;
         toppingString = toppingString + " " + toppings[i];
+       }
     }
     // total = baseCost + cost of all toppings
 
